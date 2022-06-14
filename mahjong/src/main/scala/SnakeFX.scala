3ffe8ab56647a590ca76ffaf8a6b2bda706e41c9
Pawel Pycia <pawel.pycia29@gmail.com>
2022-06-14 20:12:08 +0200

    added tiles

--- mahjong/src/main/scala/SnakeFX.scala
+++ mahjong/src/main/scala/SnakeFX.scala
@@ -9,6 +9,7 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.shape.{Box, Rectangle}
 import scalafx.Includes._
 import scalafx.event.ActionEvent
+import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout.{BorderPane, HBox}
 
@@ -22,23 +23,46 @@ object SnakeFX extends JFXApp3 {
       scene = new Scene(900, 700) {
 
 
-        content = pyramid(0, 0) ++ pyramid(200, 0) ++ pyramid(0, 300) ++ pyramid(200, 300) ++ longStrip(50, 175) ++
-           List(square(0, 200, Green), square(300, 200, Green))
+        content = pyramid(0, 0) ++ pyramid(200, 0) ++ pyramid(0, 300) ++ pyramid(200, 300)
+
 
 
       }
     }
   }
 
-  def pyramid(x: Double, y: Double) =
+  def pyramid(x: Double, y: Double): Seq[ImageView] =
     for(i <- 0 until 3;
         j <- 0 until 3)
-    yield square(x + i * 50, y + j * 50, Red)
+//    yield square(x + i * 50, y + j * 50, Red)
+    yield tileImage(x + i * 50, y + j * 60, 1)
 
-  def longStrip(x: Double, y: Double) =
+  private def longStrip(x: Double, y: Double): Seq[Rectangle] =
     for (i <- 0 until 5;
          j <- 0 until 2)
-      yield square(x + i * 50, y + j * 50, Green)
+      yield square(x + i * 50, y + j * 60, Green)
+
+
+  def tileImage(xr: Double, yr: Double, img: Int) = new ImageView {
+    x = xr
+    y = yr
+    private val img: Int = img
+    private var shadow = 0
+    var count = img
+    image = new Image(s"file:src/main/scala/mahjong_tiles/tiles-${getNumber(img)}-${switchShadow(shadow)}.png")
+    onMouseClicked = () => {
+      shadow = 1 - shadow
+      image = new Image(s"file:src/main/scala/mahjong_tiles/tiles-${getNumber(img)}-${switchShadow(shadow)}.png")
+    }
+  }
+
+  def getNumber(number: Int): String = {
+    if (number > 9) s"$number" else s"0$number"
+  }
+
+  def switchShadow(shadow: Int) = {
+    if (shadow == 0) "00" else "01"
+  }
 
 
   def square(xr: Double, yr: Double, color: Color) = new Rectangle {
